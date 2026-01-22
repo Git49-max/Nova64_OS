492c66e172c08fde961725d9a5318442c01e8fcc
SauloDev <doroteios@outlook.com>
2026-01-22 17:30:54 -0300

    Add Global Descriptor Table for Nova64 OS

---
+++ gdt.asm
@@ -0,0 +1,22 @@
+;Global Descriptor Table Nova64 OS. Originally wrote by Saulo Henrique in ;Thursday, January 22nd, 2026.
+
+;Last update: Thursday, January 22nd, ;2026, at 17:16 GMT-3 (Horário de Brasília)
+
+;gdt.asm
+
+gdt_start:
+    dq 0x0
+gdt_code:
+    dw 0xffff, 0x0
+    db 0x0, 10011010b, 11001111b, 0x0
+gdt_data:
+    dw 0xffff, 0x0
+    db 0x0, 10010010b, 11001111b, 0x0
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
