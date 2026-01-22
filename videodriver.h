38abd2d5f8fa08f42960d231eb456e6df96d8906
SauloDev <doroteios@outlook.com>
2026-01-22 18:41:12 -0300

    Add header comments to videodriver.h
    Added header comments with author and update information.

--- videodriver.h
+++ videodriver.h
@@ -1,3 +1,10 @@
+/*Video driver header for Nova64 OS (Text mode). Originally wrote by Saulo Henrique in Thursday, January 22nd, 2026.
+
+Last update: Thursday, January 22nd, 2026, at 18:40 GMT-3 (Horário de Brasília)
+
+videodriver.h*/
+
+
 #ifndef VIDEODRIVER_H
 #define VIDEODRIVER_H
 void putc(char letter, unsigned char color, int x, int y);
