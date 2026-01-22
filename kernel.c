06c947ed3c1d71b23ebd60964bac9281b553bb81
SauloDev <doroteios@outlook.com>
2026-01-22 19:31:12 -0300

    Fix include syntax and update last modified date
    Updated last update timestamp and fixed include syntax.

--- kernel.c
+++ kernel.c
@@ -1,10 +1,10 @@
 /*Basic kernel for Nova64 OS. Originally wrote by Saulo Henrique in Thursday, January 22nd, 2026.
 
-Last update: Thursday, January 22nd, 2026, at 19:23 GMT-3 (Horário de Brasília)
+Last update: Thursday, January 22nd, 2026, at 19:31 GMT-3 (Horário de Brasília)
 
 kernel.c*/
 
-#include videodriver.h
+#include "videodriver.h"
 void main(){
   print("Nova64 is here!", 0x02, 0, 0);
   putc('>', 0x02, 0, 1);
