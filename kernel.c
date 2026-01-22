29b777220db6de975dc62ec4a5243235ab5f8b75
SauloDev <doroteios@outlook.com>
2026-01-22 18:36:47 -0300

    Implement basic kernel functionality in kernel.c

---
+++ kernel.c
@@ -0,0 +1,8 @@
+#include videodriver.h
+void main(){
+  print("Nova64 is here!", 0x02, 0, 0);
+  putc('>', 0x02, 0, 1);
+  print("Sistem ready to further development", 0x02, 1, 1)
+  while(1){
+  }
+}
