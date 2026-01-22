d381b0c86564a116daea3e103cebec0293b404d4
SauloDev <doroteios@outlook.com>
2026-01-22 19:23:19 -0300

    Update last update timestamp and fix print statement

--- kernel.c
+++ kernel.c
@@ -1,6 +1,6 @@
 /*Basic kernel for Nova64 OS. Originally wrote by Saulo Henrique in Thursday, January 22nd, 2026.
 
-Last update: Thursday, January 22nd, 2026, at 18:39 GMT-3 (Horário de Brasília)
+Last update: Thursday, January 22nd, 2026, at 19:23 GMT-3 (Horário de Brasília)
 
 kernel.c*/
 
@@ -8,7 +8,7 @@ kernel.c*/
 void main(){
   print("Nova64 is here!", 0x02, 0, 0);
   putc('>', 0x02, 0, 1);
-  print("Sistem ready to further development", 0x02, 1, 1)
+  print("Sistem ready to further development", 0x02, 1, 1);
   while(1){
   }
 }
