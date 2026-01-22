8750ee81b9e446a964e82071e95472c986fcc0c1
SauloDev <doroteios@outlook.com>
2026-01-22 18:39:33 -0300

    Add initial kernel code for Nova64 OS

--- kernel.c
+++ kernel.c
@@ -1,3 +1,9 @@
+/*Basic kernel for Nova64 OS. Originally wrote by Saulo Henrique in Thursday, January 22nd, 2026.
+
+Last update: Thursday, January 22nd, 2026, at 18:39 GMT-3 (Horário de Brasília)
+
+kernel.c*/
+
 #include videodriver.h
 void main(){
   print("Nova64 is here!", 0x02, 0, 0);
