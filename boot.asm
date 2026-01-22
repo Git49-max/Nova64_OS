a63791b8ab59933e545fdfe70278fc2317ecdd3e
SauloDev <doroteios@outlook.com>
2026-01-22 17:29:49 -0300

    Implement bootloader for Nova64 OS
    Added bootloader code for Nova64 OS, including kernel loading and switching to protected mode.

---
+++ boot.asm
@@ -0,0 +1,61 @@
+;Bootloader for Nova64 OS. Originally ;wrote by Saulo Henrique in Thursday, ;January 22nd, 2026.
+
+;Last update: Thursday, January 22nd, ;2026, at 17:15 GMT-3 (Horário de Brasília)
+
+;boot.asm
+
+[bits 16]
+[org 0x7c00]
+
+KERNEL_OFFSET equ 0x1000
+
+mov [BOOT_DRIVE], dl 
+
+mov bp, 0x9000
+mov sp, bp
+
+call load_kernel     
+call switch_to_pm    
+jmp $
+
+%include "gdt.asm"
+
+[bits 16]
+load_kernel:
+    mov bx, KERNEL_OFFSET
+    mov dh, 15            
+    mov dl, [BOOT_DRIVE]
+    mov ah, 0x02          
+    mov al, dh
+    mov ch, 0x00
+    mov dh, 0x00
+    mov cl, 0x02          
+    int 0x13
+    ret
+
+[bits 16]
+switch_to_pm:
+    cli
+    lgdt [gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:init_pm
+
+[bits 32]
+init_pm:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    call KERNEL_OFFSET
+    jmp $
+
+BOOT_DRIVE db 0
+times 510-($-$$) db 0
+dw 0xaa55
+
+
