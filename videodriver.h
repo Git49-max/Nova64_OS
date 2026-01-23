317a066c8395c7acae5071399b8d04ba044cdd05
SauloDev <doroteios@outlook.com>
2026-01-23 18:59:33 -0300

    Update last update date in videodriver.h

--- videodriver.h
+++ videodriver.h
@@ -1,6 +1,6 @@
 /*Video driver header for Nova64 OS (Text mode). Originally wrote by Saulo Henrique in Thursday, January 22nd, 2026.
 
-Last update: Thursday, January 22nd, 2026, at 21:07 GMT-3 (Horário de Brasília)
+Last update: Friday, January 23rd, 2026, at 18:59 GMT-3 (Horário de Brasília)
 
 videodriver.h*/
 
@@ -9,5 +9,6 @@ videodriver.h*/
 #define VIDEODRIVER_H
 void putc(char letter, unsigned char color, int x, int y);
 void print(char *str, unsigned char color, int x, int y);
+void print_int(int n, unsigned char color, int x, int y);
 void clear_screen();
 #endif
