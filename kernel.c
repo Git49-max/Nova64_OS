18bcf94d23de204ae30d3e253eed6430d719649c
SauloDev <doroteios@outlook.com>
2026-01-23 19:41:41 -0300

    Update time display position in kernel.c

--- kernel.c
+++ kernel.c
@@ -1,5 +1,5 @@
 /*Basic kernel for Nova64 OS. Originally wrote by Saulo Henrique in Thursday, January 22nd, 2026.
-Last update: Friday, January 23rd, 2026, at 18:34 GMT-3 (Horário de Brasília)
+Last update: Friday, January 23rd, 2026, at 19:41 GMT-3 (Horário de Brasília)
 
 kernel.c*/
 
@@ -13,8 +13,8 @@ void main(){
   print("System ready to further development", 0x02, 1, 1);
   while(1){
     get_time(&h, &m, &s);
-    print_int(h, 0x02, 76, 25);
-    putc(';', 0x02, 78, 25);
-    print_int(m, 0x02, 80, 25);
+    print_int(h, 0x02, 75, 24);
+    putc(';', 0x02, 77, 24);
+    print_int(m, 0x02, 79, 24);
   }
 }
