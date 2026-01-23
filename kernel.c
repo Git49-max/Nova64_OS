f205ba47ec52a0ab9bd55e4bc7bd7a2d4604d00e
SauloDev <doroteios@outlook.com>
2026-01-22 22:44:29 -0300

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -5,9 +5,8 @@ Last update: Thursday, January 22nd, 2026, at 19:31 GMT-3 (Horário de Brasília
 kernel.c*/
 
 #include "videodriver.h"
-clear_screen();
-void main()
-  
+void main(){
+  clear_screen();
   print("Nova64 is here!", 0x02, 0, 0);
   putc('>', 0x02, 0, 1);
   print("Sistem ready to further development", 0x02, 1, 1);
