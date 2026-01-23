5be3e3bd6763d05d78ad58e6c9650fe6f79e5822
SauloDev <doroteios@outlook.com>
2026-01-23 09:05:14 -0300

    Fix typo in kernel.c message

--- kernel.c
+++ kernel.c
@@ -1,6 +1,6 @@
 /*Basic kernel for Nova64 OS. Originally wrote by Saulo Henrique in Thursday, January 22nd, 2026.
 
-Last update: Thursday, January 22nd, 2026, at 19:31 GMT-3 (Horário de Brasília)
+Last update: Friday, January 23rd, 2026, at 09:04 GMT-3 (Horário de Brasília)
 
 kernel.c*/
 
@@ -9,7 +9,7 @@ void main(){
   clear_screen();
   print("Nova64 is here!", 0x02, 0, 0);
   putc('>', 0x02, 0, 1);
-  print("Sistem ready to further development", 0x02, 1, 1);
+  print("System ready to further development", 0x02, 1, 1);
   while(1){
   }
 }
