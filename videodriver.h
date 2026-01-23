5fde8db8cad43756f8c7822d33105cc5fef9734f
SauloDev <doroteios@outlook.com>
2026-01-22 21:08:16 -0300

    Add clear_screen function declaration

--- videodriver.h
+++ videodriver.h
@@ -1,6 +1,6 @@
 /*Video driver header for Nova64 OS (Text mode). Originally wrote by Saulo Henrique in Thursday, January 22nd, 2026.
 
-Last update: Thursday, January 22nd, 2026, at 18:40 GMT-3 (Horário de Brasília)
+Last update: Thursday, January 22nd, 2026, at 21:07 GMT-3 (Horário de Brasília)
 
 videodriver.h*/
 
@@ -9,4 +9,5 @@ videodriver.h*/
 #define VIDEODRIVER_H
 void putc(char letter, unsigned char color, int x, int y);
 void print(char *str, unsigned char color, int x, int y);
+void clear_screen();
 #endif
