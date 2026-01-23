b529b70315f82f6b214f610fd0bb9be72ccfa823
SauloDev <doroteios@outlook.com>
2026-01-23 15:54:18 -0300

    Refactor itoa and putc functions for clarity

--- videodriver.c
+++ videodriver.c
@@ -1,6 +1,6 @@
 /* Video driver for Nova64 OS (Text mode). Originally wrote by Saulo Henrique in Thursday, January 22nd, 2026.
 
-Last update: Friday, January 23rd, 2026, at 14:39 GMT-3 (Horário de Brasília)
+Last update: Friday, January 23rd, 2026, at 15:53 GMT-3 (Horário de Brasília)
 
 videodriver.c
 
@@ -8,49 +8,60 @@ videodriver.c
 
 #define vidAdr 0xB8000
 
-void itoa(int n, char *str){
-int i = 0;
-if (n == 0){
-str[i++] = '0';
-str[i] = '\0';}
+void itoa(int n, char *str) {
+    int i = 0;
+    if (n == 0) {
+        str[i++] = '0';
+        str[i] = '\0';
+        return;
+    }
 
-while(n > 0){
-    str[i++] = (n % 10) + '0';
-    n = n/10;
-}
-str[i] = '\0';
-int start = 0;
-int end = i - 1;
-while(start < end){
-char temp = str[start];
-str[start] = str[end];
-str[end] = temp;
-start++;
-end--;
+    while (n > 0) {
+        str[i++] = (n % 10) + '0';
+        n = n / 10;
+    }
+    str[i] = '\0';
 
-void putc(char letter, unsigned char color, int x, int y, bool ascii, char *bufferstr){
-volatile unsigned char *vram = (unsigned char*) vidAdr;
-int offset = (y * 80 + x) * 2;
-if (ascii == true){
- vram[offset] = itoa(letter, bufferstr);
- vram[offset + 1] = color;
-} else {
-vram[offset] = letter;
-vram[offset + 1] = color;
+    int start = 0;
+    int end = i - 1;
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
 }
+
+void putc(char letter, unsigned char color, int x, int y) {
+    volatile unsigned char *vram = (unsigned char*) vidAdr;
+    int offset = (y * 80 + x) * 2;
+    vram[offset] = letter;
+    vram[offset + 1] = color;
 }
-/* putc for a single caractere, now print for a string */
-void print(char *str, unsigned char color, int x, int y, bool ascii, char *bufferstr){
-int i = 0;
-while(str[i] != '\0'){
-if(x + i >= 80 && ascii == true){
-putc(str[i], color, x + i, y + i, true, bufferstr);
-} else if (x + i <= 80 && ascii == true){putc(str[i], color, x + i, y, true, bufferstr) ;}
-    else if(x + i >= 80 && ascii = false){putc(str[i], color, x + i, y + i, false, bufferstr);}
-      else putc(str[i], color, x + i, y, false, bufferstr;
-i++;
+
+void print(char *str, unsigned char color, int x, int y) {
+    int i = 0;
+    while (str[i] != '\0') {
+        int cur_x = x + i;
+        int cur_y = y;
+        
+        if (cur_x >= 80) {
+            cur_x = cur_x % 80;
+            cur_y++;
+        }
+        
+        putc(str[i], color, cur_x, cur_y);
+        i++;
+    }
 }
+
+void print_int(int n, unsigned char color, int x, int y) {
+    char buffer[12];
+    itoa(n, buffer);
+    print(buffer, color, x, y);
 }
+
 void clear_screen() {
     volatile unsigned char *vram = (unsigned char*) vidAdr;
     for (int i = 0; i < 80 * 25 * 2; i += 2) {
@@ -58,4 +69,3 @@ void clear_screen() {
         vram[i+1] = 0x07;   
     }
 }
-
