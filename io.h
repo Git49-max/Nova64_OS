05d9ed4841999fdb4581d4ada204b5ac13dd2c34
SauloDev <doroteios@outlook.com>
2026-01-23 16:54:13 -0300

    Fix header date in io.h
    Updated the date in the header comments for accuracy.

---
+++ io.h
@@ -0,0 +1,11 @@
+/* I/O header. Originally wrote by Saulo Henrique in Friday, January 23rd, 2026.
+
+Last update: Friday, January 23rd, 2026, at 16:52 GMT-3 (Horário de Brasília)
+
+io.h*/
+
+#ifndef IO_H
+#define IO_H
+void outb(unsigned short port, unsigned char val)
+unsigned char inb(unsigned short port)
+#endif
