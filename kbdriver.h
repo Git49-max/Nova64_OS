f428cb03593a8c27351a74f61c3865182f3f36b3
Saulo <doroteios@outlook.com>
2026-01-24 10:38:49 -0300

    Update keyboard driver header with new date and init function
    Updated last modification date and added keyboard_init function declaration.

--- kbdriver.h
+++ kbdriver.h
@@ -1,10 +1,11 @@
 /* Keyboard driver header. Originally wrote by Saulo Henrique in Friday, January 23rd, 2026.
 
-Last update: Friday, January 23rd, 2026, at 22:09 GMT-3 (Horário de Brasília)
+Last update: Saturday, January 24th, 2026, at 10:38 GMT-3 (Horário de Brasília)
 
 kbdriver.h*/
 
 #ifndef KBDRIVER_H
 #define KBDRIVER_H
 char get_key();
+void keyboard_init();
 #endif
