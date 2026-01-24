51da23348bbd28d22a39b702a3fb4a3a470028db
SauloDev <doroteios@outlook.com>
2026-01-23 22:10:02 -0300

    Add header comments to kbdriver.h
    Added header comments to kbdriver.h with author and date information.

---
+++ kbdriver.h
@@ -0,0 +1,10 @@
+/* Keyboard driver header. Originally wrote by Saulo Henrique in Friday, January 23rd, 2026.
+
+Last update: Friday, January 23rd, 2026, at 22:09 GMT-3 (Horário de Brasília)
+
+kbdriver.h*/
+
+#ifndef KBDRIVER_H
+#define KBDRIVER_H
+char get_key();
+#endif
