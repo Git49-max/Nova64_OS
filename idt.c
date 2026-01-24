b9190e59fdb181912c4add9449c5617505e3a914
Saulo <doroteios@outlook.com>
2026-01-24 11:55:29 -0300

    Create idt.c

---
+++ idt.c
@@ -0,0 +1,49 @@
+#include "io.h"
+
+struct idt_entry {
+    unsigned short base_low;
+    unsigned short sel;
+    unsigned char zero;
+    unsigned char flags;
+    unsigned short base_high;
+} __attribute__((packed));
+
+struct idt_ptr {
+    unsigned short limit;
+    unsigned int base;
+} __attribute__((packed));
+
+struct idt_entry idt[256];
+struct idt_ptr idtp;
+
+extern void idt_load();
+extern void irq1();
+
+void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags) {
+    idt[num].base_low = (base & 0xFFFF);
+    idt[num].base_high = (base >> 16) & 0xFFFF;
+    idt[num].sel = sel;
+    idt[num].zero = 0;
+    idt[num].flags = flags;
+}
+
+void idt_init() {
+    idtp.limit = (sizeof(struct idt_entry) * 256) - 1;
+    idtp.base = (unsigned int)&idt;
+
+    outb(0x20, 0x11);
+    outb(0xA0, 0x11);
+    outb(0x21, 0x20);
+    outb(0xA1, 0x28);
+    outb(0x21, 0x04);
+    outb(0xA1, 0x02);
+    outb(0x21, 0x01);
+    outb(0xA1, 0x01);
+    outb(0x21, 0x0);
+    outb(0xA1, 0x0);
+
+    idt_set_gate(33, (unsigned long)irq1, 0x08, 0x8E);
+
+    idt_load();
+    asm volatile("sti");
+}
