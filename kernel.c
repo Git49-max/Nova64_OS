9b9f210b7d25b68c7c82b4af35f5e239c16c8f9e
Saulo <doroteios@outlook.com>
2026-01-24 14:08:42 -0300

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -2,7 +2,6 @@
 Last update: Saturday, January 24th, 2026, at 12:12 GMT-3 (Horário de Brasília)
 
 kernel.c */
-
 #include "videodriver.h"
 #include "rtcdriver.h"
 #include "kbdriver.h"
@@ -19,26 +18,30 @@ void main() {
     idt_init();
     keyboard_init();
 
-    print("Nova64 OS - IDT Mode", 0x02, 0, 0);
+    print("Nova64 OS - IDT Keyboard Active", 0x02, 0, 0);
     putc('>', 0x02, 0, 1);
-    print(" Interrupts enabled", 0x02, 1, 1);
 
     while(1) {
         get_time(&h, &m, &s);
+        
         if (h < 10) {
             putc('0', 0x02, 71, 24);
             print_int(h, 0x02, 72, 24);
         } else {
             print_int(h, 0x02, 71, 24);
         }
+
         putc(':', 0x02, 73, 24);
+
         if (m < 10) {
             putc('0', 0x02, 74, 24);
             print_int(m, 0x02, 75, 24);
         } else {
             print_int(m, 0x02, 74, 24);
         }
+
         putc(':', 0x02, 76, 24);
+
         if (s < 10) {
             putc('0', 0x02, 77, 24);
             print_int(s, 0x02, 78, 24);
