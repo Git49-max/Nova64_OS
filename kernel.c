8d6dd204e759bb00c368815893ce539549a8319c
SauloDev <doroteios@outlook.com>
2026-01-23 22:20:04 -0300

    Modify kernel.c for keyboard input and timestamp
    Updated last update timestamp and added keyboard driver functionality.

--- kernel.c
+++ kernel.c
@@ -1,17 +1,27 @@
 /*Basic kernel for Nova64 OS. Originally wrote by Saulo Henrique in Thursday, January 22nd, 2026.
-Last update: Friday, January 23rd, 2026, at 20:15 GMT-3 (Horário de Brasília)
+Last update: Friday, January 23rd, 2026, at 22:19 GMT-3 (Horário de Brasília)
 
 kernel.c*/
 
 #include "videodriver.h"
 #include "rtcdriver.h"
+#include "kbdriver.h"
 void main(){
   clear_screen();
   int h, m, s;
   print("Nova64 is here!", 0x02, 0, 0);
   putc('>', 0x02, 0, 1);
-  print("System ready to further development", 0x02, 1, 1);
+  print(" System ready to further development", 0x02, 1, 1);
   while(1){
+    int cursor_x = 0;
+    int cursor_y = 2;
+    char tecla = get_key();
+    if(tecla != 0){
+      putc(tecla, 0x02, cursor_x, cursor_y);
+      cursor_x++;
+      if(cursor_x > 79){
+        cursor_x = 0;
+        cursor_y++;
     get_time(&h, &m, &s);
     if(h < 10){
        print_int('0', 0x02, 75, 24);
